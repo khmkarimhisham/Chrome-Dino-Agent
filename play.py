a8f93e0e94fc87909d14b4eabf8b118f87cc9718
Karim <khm.karimhisham@gmail.com>
2021-08-23 03:43:52 +0200

    v1

--- play.py
+++ play.py
@@ -4,15 +4,19 @@ from keras.models import load_model
 import numpy as np
 import mss
 from PIL import Image
+import pyautogui
 
 model = load_model('model_file.h5')
 
 while(True):
   
-    monitor = {"top": 130, "left": 650, "width": 650, "height": 150}
+    monitor = {"top": 200, "left": 650, "width": 600, "height": 130}
     img = np.array(mss.mss().grab(monitor))
     img_gray = Image.fromarray(img).convert('L')
     x = np.expand_dims(img_gray, axis=(0, 3))
     prediction = np.argmax(model.predict(x), axis=-1)[0]
+    if prediction == 2:
+        pyautogui.press('up')
+    elif prediction == 0:
+        pyautogui.press('down')
 
-    print(prediction)

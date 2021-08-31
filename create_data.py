c6a978ff14bf6dbeb7beb10e8430abec93b0e385
Karim <khm.karimhisham@gmail.com>
2021-08-31 10:12:08 +0200

    v 2.0

--- create_data.py
+++ create_data.py
@@ -3,14 +3,13 @@ import numpy as np
 from PIL import Image
 import os
 import keyboard
-from random import random
 import mss
 
 data_file = "data/"
 up_file = "data/up/"
 down_file = "data/down/"
-nokey_file = "data/nokey/"
-monitor = {"top": 200, "left": 650, "width": 600, "height": 130}
+right_file = "data/right/"
+monitor = {"top": 205, "left": 740, "width": 115, "height": 100}
 
 
 if not os.path.isdir(data_file):
@@ -19,45 +18,34 @@ if not os.path.isdir(data_file):
         os.mkdir(up_file)
     if not os.path.isdir(down_file):
         os.mkdir(down_file)
-    if not os.path.isdir(nokey_file):
-        os.mkdir(nokey_file)
-
-i = 1
+    if not os.path.isdir(right_file):
+        os.mkdir(right_file)
 
 
 def up():
-    global i
     screenshot = np.array(mss.mss().grab(monitor))
     img = Image.fromarray(screenshot).convert('L')
     img.save(up_file + str(time.time()) + '.png')
-    i += 1
 
 
 def down():
-    global i
     screenshot = np.array(mss.mss().grab(monitor))    
     img = Image.fromarray(screenshot).convert('L')
     img.save(down_file + str(time.time()) + '.png')
-    i += 1
 
 
-def nokey():
-    global i
-    while True:
-        screenshot = np.array(mss.mss().grab(monitor))    
-        img = Image.fromarray(screenshot).convert('L')
-        img.save(nokey_file + str(time.time()) + '.png')
-        i += 1
-        time.sleep(0.5)
+def right():
+    screenshot = np.array(mss.mss().grab(monitor))    
+    img = Image.fromarray(screenshot).convert('L')
+    img.save(right_file + str(time.time()) + '.png')
 
 
 keyboard.on_press_key("up", lambda _: up())
 keyboard.on_press_key("down", lambda _: down())
+keyboard.on_press_key("right", lambda _: right())
 keyboard.on_press_key("q", lambda _: os._exit(0))
 
+print('The game has started')
+
 while True:
-    print("Press space to start")
-    if keyboard.read_key() == "space":
-        print('The game has started')
-        nokey()
-        
+    pass        
--- play.py
+++ play.py
@@ -10,13 +10,18 @@ model = load_model('model_file.h5')
 
 while(True):
   
-    monitor = {"top": 200, "left": 650, "width": 600, "height": 130}
+    # monitor = {"top": 205, "left": 740, "width": 115, "height": 100}
+    monitor = {"top": 139, "left": 740, "width": 115, "height": 100}
     img = np.array(mss.mss().grab(monitor))
     img_gray = Image.fromarray(img).convert('L')
     x = np.expand_dims(img_gray, axis=(0, 3))
     prediction = np.argmax(model.predict(x), axis=-1)[0]
     if prediction == 2:
         pyautogui.press('up')
+        print('up')
     elif prediction == 0:
         pyautogui.press('down')
+        print('down')
+    elif prediction == 1:
+        print('right')
 
